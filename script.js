407b03fcebf7f6f631f0bff12dc289f2ce091642
Jimmi is Coding <jimmicodathon@gmail.com>
2021-04-29 13:18:13 -0500

    Incrementally deshades squares by 10%.  Could use more CSS and user options in the future.

--- script.js
+++ script.js
@@ -1,19 +1,54 @@
-let divGrid = document.getElementById( "grid" );
-
-function createGrid( squares ) {
-    let rowOfSquares = document.createElement( "div" );
-    rowOfSquares.setAttribute( "class", "rows" );
-
-    for ( let i=0; i<squares; i++ ) {
-        let square = document.createElement( "div" );
-        square.setAttribute( "class", "squares" );
-        rowOfSquares.appendChild( square )
+function createGrid() {
+    const squares = document.getElementById( "size" ).value;
+    document.getElementById( "size-display" ).innerText = "Grid Size = " + squares + "x" + squares;
+    const totalSquares = squares * squares;
+    let divGrid = document.getElementById( "grid" );
+    while( divGrid.firstChild ){
+        divGrid.removeChild( divGrid.firstChild );
     }
 
-    for ( let i=0; i<squares; i++ ) {
-        divGrid.appendChild( rowOfSquares.cloneNode( true ) )
+    const square = document.createElement( "div" );
+    square.setAttribute( "class", "squares" );
+    // I can't seem to add the event listener until after it appends the cloneNodes
+        // cloning a node can not clone event listeners even those set on element properties, i.e onclick
+
+    for ( let i=0; i<totalSquares; i++ ) {
+        divGrid.appendChild( square.cloneNode( true ) );
+    }
+    divGrid.style.gridTemplateColumns = "repeat( " + squares + ", 1fr )";
+    //divGrid.style.gridtemplaterows = "auto"; // seems to default to this anyway
+    addListeners();
+}
+
+
+function addListeners() {
+    let gridOfSquares = document.getElementsByClassName( "squares" );
+    for ( let i=0; i<gridOfSquares.length ; i++ ) {
+        gridOfSquares[i].addEventListener( "mouseenter", addColor )
     }
 }
 
-createGrid(16);
-console.log(divGrid);
+
+function addColor( e ) {
+    const squareColor = window.getComputedStyle( e.target ).getPropertyValue( "background-color" );
+    const rgbDigits = squareColor.match( /\d+/g);
+    console.log( rgbDigits );
+    let rgbPercents = [];
+    //NEED TO CONVERT TO PERCENTS AND ROUND TO THE NEAREST INTEGER AND ADD 10% UNTIL WHITE
+    for ( let i=0; i<rgbDigits.length; i++ ) {
+        rgbPercents.push( Math.round( ( rgbDigits[i]/255 * 100 ) + 10 ) ) ;
+    }
+    console.log( rgbPercents )
+
+    if ( squareColor !== "rgb(255, 255, 255)" ) {
+        e.target.style.backgroundColor = "rgb( " + rgbPercents[0] + "%, " + rgbPercents[1] + "%, " + rgbPercents[2] + "% )";
+    }
+    /* black to white
+    if ( e.target.style.backgroundColor == "black" || e.target.style.backgroundColor == "" ) {
+        e.target.style.backgroundColor = "white";
+    }
+    */
+}
+
+
+createGrid();
